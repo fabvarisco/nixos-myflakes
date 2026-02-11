38478e24cb127ea26ce364d9961e62b1f5bdc433
Fabricio Varisco <fabricio.varisco@outlook.com>
2026-02-11 18:39:45 -0300

    fix home

--- home.nix
+++ home.nix
@@ -35,13 +35,37 @@
    '';
  };
 
- home.file.".config/hypr".source = ./config/hypr;
- home.file.".config/wofi".source = ./config/wofi;
- home.file.".config/waybar".source = ./config/waybar;
- home.file.".config/kitty".source = ./config/kitty;
- home.file.".config/btop".source = ./config/btop;
+ # Hyprland - arquivos estáticos (colors.conf e hyprlock.conf são gerados pelo theme-switcher)
+ home.file.".config/hypr/hyprland.conf".source = ./config/hypr/hyprland.conf;
+ home.file.".config/hypr/hyprpaper.conf".source = ./config/hypr/hyprpaper.conf;
+ home.file.".config/hypr/monitors.conf".source = ./config/hypr/monitors.conf;
+ home.file.".config/hypr/workspaces.conf".source = ./config/hypr/workspaces.conf;
+ home.file.".config/hypr/change-wallpaper.sh".source = ./config/hypr/change-wallpaper.sh;
+ home.file.".config/hypr/wallpaper-slideshow.sh".source = ./config/hypr/wallpaper-slideshow.sh;
+ home.file.".config/hypr/wallpaper-wofi.sh".source = ./config/hypr/wallpaper-wofi.sh;
+ home.file.".config/hypr/scripts".source = ./config/hypr/scripts;
+
+ # Wofi - apenas config estático (style.css é gerado pelo theme-switcher)
+ home.file.".config/wofi/config".source = ./config/wofi/config;
+
+ # Waybar - config estático e scripts (style.css é gerado pelo theme-switcher)
+ home.file.".config/waybar/config.jsonc".source = ./config/waybar/config.jsonc;
+ home.file.".config/waybar/scripts".source = ./config/waybar/scripts;
+
+ # Kitty - arquivos estáticos (theme.conf é gerado pelo theme-switcher)
+ home.file.".config/kitty/kitty.conf".source = ./config/kitty/kitty.conf;
+ home.file.".config/kitty/mocha.conf".source = ./config/kitty/mocha.conf;
+
+ # Btop - apenas os temas (btop.conf é modificado pelo theme-switcher)
+ home.file.".config/btop/themes".source = ./config/btop/themes;
+
  home.file.".config/starship.toml".source = ./config/starship.toml;
- home.file.".config/swaync".source = ./config/swaync;
+
+ # SwayNC - config estático e widgets (style.css é gerado pelo theme-switcher)
+ home.file.".config/swaync/config.json".source = ./config/swaync/config.json;
+ home.file.".config/swaync/widgets.css".source = ./config/swaync/widgets.css;
+
+ # Themes - templates e definições para o theme-switcher
  home.file.".config/themes".source = ./config/themes;
 
  # SDDM theme 
