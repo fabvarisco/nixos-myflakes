3c78c2e32bdc5a11aaa6f2f47358e1f59380601c
Fabricio Varisco <fabricio.varisco@outlook.com>
2026-02-13 14:39:13 -0300

    fix yazi

--- home.nix
+++ home.nix
@@ -43,6 +43,7 @@
  home.file.".config/starship.toml".source = ./config/starship.toml;
  home.file.".config/swaync".source = ./config/swaync;
  home.file.".config/yazi".source = ./config/yazi;
+ home.file.".config/yazi".recursive = true;
 
  # SDDM theme 
  home.file.".config/sddm-theme/Main.qml".source = ./config/sddm-theme/Main.qml;
