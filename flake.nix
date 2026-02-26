a89867496b7f843ad5a524ef463987841648b340
Fabricio Varisco Oliveira <fabricio.varisco@outlook.com>
2026-02-26 09:30:02 -0300

    fix flake

--- flake.nix
+++ flake.nix
@@ -1,47 +1,66 @@
 {
- description="hyprland";
- inputs = {
-   nixpkgs.url = "nixpkgs/nixos-unstable";
-   home-manager = {
+  description = "hyprland";
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org"
+      "https://hyprland.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCUSjDg="
+    ];
+  };
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
-   };
-   silent-sddm = {
+    };
+
+    silent-sddm = {
       url = "github:uiriansan/SilentSDDM";
       inputs.nixpkgs.follows = "nixpkgs";
-   };
-   zen-browser = {
+    };
+
+    zen-browser = {
       url = "github:youwen5/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-   vicinae = {
+
+    vicinae = {
       url = "github:vicinaehq/vicinae";
+      inputs.nixpkgs.follows = "nixpkgs"; # evita segundo nixpkgs
     };
-   hyprswitch = {
+
+    hyprswitch = {
       url = "github:H3rmt/hyprswitch";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = {nixpkgs, home-manager, silent-sddm, vicinae, ... }@inputs:{
-   nixosConfigurations.thinkpad-hypr = nixpkgs.lib.nixosSystem {
-     system = "x86_64-linux";
-     specialArgs = { inherit inputs; };
-     modules = [
-       ./configuration.nix
-       silent-sddm.nixosModules.default
-        home-manager.nixosModules.home-manager {
-        home-manager =
+  outputs = { nixpkgs, home-manager, silent-sddm, vicinae, ... } @inputs: {
+      nixosConfigurations.thinkpad-hypr = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+        silent-sddm.nixosModules.default
+        home-manager.nixosModules.home-manager
         {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users.fabvarisco = import ./home.nix;
-              backupFileExtension = "backup";
-              extraSpecialArgs = { inherit inputs; };
-              };
-         }
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.fabvarisco = import ./home.nix;
+            backupFileExtension = "backup";
+            extraSpecialArgs = { inherit inputs; };
+          };
+        }
       ];
-   };
-}; 
-
+    };
+  };
 }
