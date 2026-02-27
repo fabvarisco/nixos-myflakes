85b944e7bf7ca74c76796ec90c5c3bcf09cc6fa1
Fabricio Varisco Oliveira <fabricio.varisco@outlook.com>
2026-02-27 15:50:40 -0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -98,8 +98,9 @@
 
   # Hyprland
   programs.hyprland = {
-     enable = true;
-     xwayland.enable = true;
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    xwayland.enable = true;
   };
 
   # Enable X11 for SDDM
@@ -267,6 +268,29 @@
   ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];   
+  
+   substituters = [
+    "https://cache.nixos.org"
+    "https://hyprland.cachix.org"
+    "https://nix-community.cachix.org"
+  ];
+
+  trusted-public-keys = [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCUSjDg="
+  ];
+
+
+  # Limitar jobs paralelos para evitar OOM
+  nix.settings.max-jobs = 2;
+  nix.settings.cores = 2;
+  
+  # Habilitar swap se não tiver
+  swapDevices = [ 
+    { device = "/swapfile"; size = 8192; } # 8GB swap
+  ];
+
 
   system.stateVersion = "25.05";
 
