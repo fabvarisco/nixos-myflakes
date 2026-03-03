4b5605279ffedbccb6b55fd73ed1657ee1cdbd2e
Fabricio Varisco <fabricio.varisco@outlook.com>
2026-03-02 22:19:37 -0300

    fix bugs

--- home.nix
+++ home.nix
@@ -116,6 +116,7 @@
     settings = {
       close_on_focus_loss = true;
       pop_to_root_on_close = true;
+      terminal = "kitty";
       font.normal = {
         size = 10;
         family = "JetBrains Mono Nerd Font";
