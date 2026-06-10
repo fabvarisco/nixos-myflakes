36a003f955b7c1739a5b9c99d3e5ba74cd04bd18
Fabricio Varisco Oliveira <fabricio.varisco@outlook.com>
2026-06-09 21:19:39 -0300

    wip

--- modules/common.nix
+++ modules/common.nix
@@ -61,6 +61,11 @@
     profileIcons.fabvarisco = ../config/profile-pics/profile_2.jpg;
   };
 
+  # Gnome Keyring (needed for VS Code and other apps on non-GNOME desktops)
+  services.gnome.gnome-keyring.enable = true;
+  security.pam.services.login.enableGnomeKeyring = true;
+  security.pam.services.sddm.enableGnomeKeyring = true;
+
   # Audio
   security.rtkit.enable = true;
   services.pipewire = {
